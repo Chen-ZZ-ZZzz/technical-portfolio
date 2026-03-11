44e6e779e0581d5b08fe8fd30206fd7f71ad205f
Chen-ZZ-ZZzz <deva.calibre@googlemail.com>
2026-03-11 11:46:22 +0100

    Refactor streak calculation logic in streak_chance.py

--- Python/streak_chance..py
+++ Python/streak_chance..py
@@ -2,24 +2,29 @@ import random
 
 number_of_streaks = 0
 
-for experiment_number in range(10000):  # Run 100,000 experiments total.
+for experiment_number in range(10000):
+    results = []
+    counter_0 = 0
+    counter_1 = 0
 
-    # Code that creates a list of 100 'heads' or 'tails' values
-    countcoin = ''
-    for i in range(100):       # flip coins 100 times
-        if random.randint(0, 1) == 0: # tails
-            countcoin += 'T'
-        else:                   # heads
-            countcoin += 'H'
+    for i in range(0, 100):
+        results.append(random.randint(0, 1))
 
-    # Code that checks if there is a streak of 6 heads or tails in a row
-    countsame = 1               # It's also easy to make an off-by-one error because remember that an H or T by itself is a streak of length 1, not of length 0.
-    for i in range(99):
-        if countcoin[i] == countcoin[i+1]:
-            countsame += 1
+    for result in results:
+        if result == 0:
+            counter_0 += 1
+            counter_1 = 0
+
+            if counter_0 == 6:
+                number_of_streaks += 1
+                break
+
         else:
-            countsame = 1
-        if countsame == 6:
-            number_of_streaks += 1
+            counter_1 += 1
+            counter_0 = 0
 
-print('Chance of streak: %s%%' % (number_of_streaks / 100))
+            if counter_1 == 6:
+                number_of_streaks += 1
+                break
+
+print('Chance of streak: %s%%' % (number_of_streaks / 100)
